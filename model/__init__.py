51fa4e112af937b16015d94f50af89ecd0a44028
chenbo <1126531273@qq.com>
2018-07-28 15:17:56 +0800

    [A] 配置sqlalchemy初始化设置

---
+++ model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Chenbo
+@time: 2018/7/28 11:53
+"""
+from flask_sqlalchemy import SQLAlchemy
+
+from run import app
+
+db = SQLAlchemy(app)
