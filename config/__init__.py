a68289cfa82eb0df015690b7c3093cd231a45552
chenbo <1126531273@qq.com>
2018-07-28 14:32:59 +0800

    [A] 初始化运行环境

---
+++ config/__init__.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Chenbo
+@time: 2018/7/28 11:53
+"""
+
+
+class Config(object):
+    """Base config class."""
+    pass
+
+
+class ProdConfig(Config):
+    """Production config class."""
+    pass
+
+
+class DevConfig(Config):
+    """Development config class."""
+    DEBUG = True
+
+
+# #!/usr/bin/env python
+# # encoding: utf-8
+#
+# """
+# @author: Chenbo
+# @time: 2018/7/28 11:53
+# """
+#
+# import os
+#
+# """
+# 环境开关   prod  test  local
+# """
+# config = str(os.environ.get("CONFIG", 'dev')).strip()
+#
+#
+# if config == "prod":
+#     from config.prod import *
+# elif config == "test":
+#     from config.test import *
+# else:
+#     from config.dev import *
