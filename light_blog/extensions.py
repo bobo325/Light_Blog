07135c8feb2e75d223023ba529afa8747ff67370
chenbo <1126531273@qq.com>
2018-08-03 14:21:15 +0800

    [U] 配置文件

---
+++ light_blog/extensions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: Chenbo
+@time: 2018/7/28 11:53
+"""
+from flask_bcrypt import Bcrypt
+
+# Create the Flask-Bcrpyt's instance
+bcrypt = Bcrypt()
