c6dac628991da856d6d5b5dec11871cd57fb7766
chenbo <1126531273@qq.com>
2018-08-03 14:21:34 +0800

    [U] 配置文件

--- light_blog/forms.py
+++ light_blog/forms.py
@@ -6,9 +6,11 @@
 @time: 2018/7/28 11:53
 """
   # 更具有安全性
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+from light_blog.model.user import User
 
 
 class CommentForm(FlaskForm):
@@ -20,3 +22,47 @@ class CommentForm(FlaskForm):
         'Name',
         validators=[DataRequired(), Length(max=255)])
     text = StringField(u'Comment', validators=[DataRequired()]) # Text deprecated
+
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+    username = StringField('Username', [DataRequired(), Length(max=255)])
+    password = PasswordField('Password', [DataRequired()])
+
+    def validate(self):
+        """Validator for check the account information."""
+        check_validate = super(LoginForm, self).validate()
+        if not check_validate:
+            return False
+
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            self.username.errors.append('Invalid username or password.')
+            return False
+
+        if not user.check_password(self.password.data):
+            self.username.errors.append('Invalid username or password.')
+            return False
+        return True
+
+
+class RegisterForm(FlaskForm):
+    """Register Form"""
+    username = StringField('Username', [DataRequired(), Length(max=255)])
+    password = PasswordField('Password', [DataRequired(), Length(min=8)])
+    comfirm = PasswordField('Confirm Password', [DataRequired(), EqualTo('password')])
+    recaptcha = RecaptchaField()
+
+    def validate(self):
+        check_validate = super(RegisterForm, self).validate()
+
+        # If validator no pass
+        if not check_validate:
+            return False
+
+        # Check the user whether already exist.
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            self.username.errors.append('User with that name already exists.')
+            return False
+        return True
