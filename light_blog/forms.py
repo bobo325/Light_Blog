4caf6b84963adad2da001443ffd65c4c4095a870
chenbo <1126531273@qq.com>
2018-08-06 17:20:20 +0800

    [U] 修改

--- light_blog/forms.py
+++ light_blog/forms.py
@@ -7,7 +7,7 @@
 """
   # 更具有安全性
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 from light_blog.model.user import User
@@ -28,6 +28,7 @@ class LoginForm(FlaskForm):
     """Login Form"""
     username = StringField('Username', [DataRequired(), Length(max=255)])
     password = PasswordField('Password', [DataRequired()])
+    remember = BooleanField('Remember Me')
 
     def validate(self):
         """Validator for check the account information."""
@@ -41,7 +42,7 @@ class LoginForm(FlaskForm):
             return False
 
         if not user.check_password(self.password.data):
-            self.username.errors.append('Invalid username or password.')
+            self.password.errors.append('Invalid username or password.')
             return False
         return True
 
@@ -51,7 +52,7 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', [DataRequired(), Length(max=255)])
     password = PasswordField('Password', [DataRequired(), Length(min=8)])
     comfirm = PasswordField('Confirm Password', [DataRequired(), EqualTo('password')])
-    # recaptcha = RecaptchaField()
+    # recaptcha = RecaptchaField()  # TODO 正式环境还要修改应用域名
 
     def validate(self):
         check_validate = super(RegisterForm, self).validate()
