8255b7d2994dc8b1b1998e05837a2d7e047ad3a4
chenbo <1126531273@qq.com>
2018-08-09 18:29:04 +0800

    [U] 取消任务调度

--- light_blog/tasks.py
+++ light_blog/tasks.py
@@ -14,45 +14,44 @@ from light_blog.extensions import flask_celery
 from light_blog.model.reminder import Reminder
 
 
-@flask_celery.task(   # 了解到该命令中-A参数表示的是Celery APP的名称，这个实例中指的就是tasks.py,
-    # 后面的tasks就是APP的名称，worker是一个执行任务角色，后面的loglevel=info记录日志类型默认是info,
-    # 这个命令启动了一个worker,用来执行程序中add这个加法任务（task）
-    bind=True,
-    igonre_result=True,
-    default_retry_delay=300,
-    max_retries=5)
-def remind(self, primary_key):
-    """Send the remind email to user when registered.
-       Using Flask-Mail.
-    """
-
-    reminder = Reminder.query.get(primary_key)
-
-    msg = MIMEText(reminder.text)
-    msg['Subject'] = 'Register Success，Congratulations!'
-    msg['FROM'] = ACCOUNT
-    msg['To'] = reminder.email
-    print("正在发送邮件！！！")
-    try:
-        smtp_server = smtplib.SMTP('smtp.qq.com')    # 亲测有效
-        # smtp_server.starttls()
-        smtp_server.login(ACCOUNT, PASSWORD)    # TODO secret care
-        smtp_server.sendmail(ACCOUNT,
-                             [reminder.email],
-                             msg.as_string())
-        smtp_server.close()
-        return
-
-    except Exception as err:
-        self.retry(exc=err)
+# @flask_celery.task(   # 了解到该命令中-A参数表示的是Celery APP的名称，这个实例中指的就是tasks.py,
+#     # 后面的tasks就是APP的名称，worker是一个执行任务角色，后面的loglevel=info记录日志类型默认是info,
+#     # 这个命令启动了一个worker,用来执行程序中add这个加法任务（task）
+#     bind=True,
+#     igonre_result=True,
+#     default_retry_delay=300,
+#     max_retries=5)
+# def remind(self, primary_key):
+#     """Send the remind email to user when registered.
+#        Using Flask-Mail.
+#     """
+#     print("Task is ready!")
+#     reminder = Reminder.query.get(primary_key)
+#
+#     msg = MIMEText(reminder.text)
+#     msg['Subject'] = 'Register Success，Congratulations!'
+#     msg['FROM'] = ACCOUNT
+#     msg['To'] = reminder.email
+#     print("正在发送邮件！！！")
+#     try:
+#         smtp_server = smtplib.SMTP('smtp.qq.com')    # 亲测有效
+#         # smtp_server.starttls()
+#         smtp_server.login(ACCOUNT, PASSWORD)    # TODO secret care
+#         smtp_server.sendmail(ACCOUNT,
+#                              [reminder.email],
+#                              msg.as_string())
+#         smtp_server.close()
+#         return
+#
+#     except Exception as err:
+#         self.retry(exc=err)
 
 
 def on_reminder_save(mapper, connect, self):
     """Callbask for task remind."""
-    remind.apply_async(args=(self.id,), eta=self.date)
+    print("I am working!")
+    # remind.apply_async(args=(self.id,), eta=self.date)
 
-
-if __name__ == '__main__':
     msg = MIMEText("多用户发送测试！")
     msg['Subject'] = 'Register Success，Congratulations!'
     msg['FROM'] = ACCOUNT
@@ -70,3 +69,4 @@ if __name__ == '__main__':
 
     except Exception as err:
         print("nice to meet you!")
+
